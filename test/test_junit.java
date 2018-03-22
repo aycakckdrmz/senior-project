63b9f595a14326062b758400fbd03732a6d8734f
aycakckdrmz <26678720+aycakckdrmz@users.noreply.github.com>
2018-03-22 01:08:05 -0400

    +main, gameloop, gamewindow, first thread, start/stop funtions

--- test/test_junit.java
+++ test/test_junit.java
@@ -1,10 +1,13 @@
 import groovy.util.GroovyTestCase;
+import org.junit.Test;
 
 class test_junit extends GroovyTestCase {
+    /*
     int sum(int a, int b){return a+b;}
 
+    @Test
     void test_assert(){
         assertEquals(sum(5,42), 47);
     }
-
+*/
 }
--- test/test_junitTest.groovy
+++
@@ -1,2 +0,0 @@
-class test_junitTest extends groovy.util.GroovyTestCase {
-}
