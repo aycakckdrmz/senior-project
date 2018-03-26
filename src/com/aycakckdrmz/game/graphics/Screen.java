778cc3808adca4b43782ea96258b49f1a4b814fa
aycakckdrmz <26678720+aycakckdrmz@users.noreply.github.com>
2018-03-25 21:32:16 -0400

    bounds handled

--- src/com/aycakckdrmz/game/graphics/Screen.java
+++ src/com/aycakckdrmz/game/graphics/Screen.java
@@ -5,23 +5,25 @@ public class Screen {
     private  int width, height;
     public int [] pixels;
 
-    int time = 0, counter = 0;
+    int xtime = 0, ytime = 0, counter = 0;
 
 
     public Screen(int width, int height){
         this.width = width;
         this.height = height;
-        pixels = new int[width * height];
+        pixels = new int[width * height]; //54400
     }
 
     public void render(){
         counter++;
-        if (counter % 100 == 0){
-            time++;
-        }
+        if (counter % 100 == 0) xtime--;
+        if (counter % 100 == 0) ytime--;
+
         for (int y = 0; y < height; y++){
+            if(ytime < 0 || ytime >= height) break;
             for(int x = 0; x < width; x++){
-                pixels[ time + time * width] = 0xff00ff;
+                if(xtime < 0 || xtime >= width) break;
+                pixels[ xtime + ytime * width] = 0xff00ff;
             }
         }
 
